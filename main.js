40841b5cf813a7153e93a0b0a9f3143e2ee28bbe
NidhiSurenu <81507816+NidhiSurenu@users.noreply.github.com>
2021-04-02 12:06:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,3 +27,59 @@ function new_image(get_image){
         canvas.add(block_object);
     });
 }
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+    keypressed=e.keyCode;
+    if(e.shiftKey==true && keypressed =='80'){
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById("current_width").innerHTML=block_width
+        document.getElementById("current_height").innerHTML=block_height
+}
+if(e.shiftKey==true && keypressed =='77'){
+    block_width=block_width-10;
+    block_height=block_height-10;
+    document.getElementById("current_width").innerHTML=block_width
+    document.getElementById("current_height").innerHTML=block_height
+}
+    if(keypressed=='70'){
+    new_image('ironman_face.png');
+    console.log("f")
+}
+if(keypressed=='66'){
+    new_image('spiderman_body.png');
+    console.log("b")
+}
+if(keypressed=='76'){
+    new_image('hulk_legs.png');
+    console.log("l")
+}
+if(keypressed=='82'){
+    new_image('hulk_right_hand');
+    console.log("r")
+}
+if(keypressed=='72'){
+    new_image('captain_america_left_hand.png');
+    console.log("h")
+}
+if(keypressed=='38')
+{
+    up();
+    console.log("up");
+}
+if(keypressed=='40')
+{
+    down();
+    console.log("down");
+}
+if(keypressed=='39')
+{
+    right();
+    console.log("right");
+}
+if(keypressed=='37')
+{
+    left();
+    console.log("left");
+}
+}
